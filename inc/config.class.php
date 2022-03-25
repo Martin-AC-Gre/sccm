ca76f5c350620a42ad714b981f1ad9e05d7003c9
Cédric Anne <cedric.anne@gmail.com>
2022-03-25 11:40:57 +0100

    Drop useless check

--- inc/config.class.php
+++ inc/config.class.php
@@ -82,7 +82,7 @@ class PluginSccmConfig extends CommonDBTM {
 
       $default_charset = DBConnection::getDefaultCharset();
       $default_collation = DBConnection::getDefaultCollation();
-      $default_key_sign = method_exists('DBConnection', 'getDefaultPrimaryKeySignOption') ? DBConnection::getDefaultPrimaryKeySignOption() : '';
+      $default_key_sign = DBConnection::getDefaultPrimaryKeySignOption();
 
       $table = 'glpi_plugin_sccm_configs';
 
